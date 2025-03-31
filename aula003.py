31455bc4c2f2dc8b21fe763684eab827c8e08843
GabrielDumith <gabriel.dumith321@gmail.com>
2025-03-31 15:43:41 -0300

    quatro operação

---
+++ aula003.py
@@ -0,0 +1,8 @@
+n1=float(input("escreva o primeiro numero: "))
+n2=float(input("escreva o segundo numero: "))
+soma=n1+n2
+subitracao=n1-n2
+mul=n1*n2
+div=n1/n2
+
+print(f"soma deu: {soma}, subtração:{subitracao}, multiplicação:{mul}, sua divisão{div}")
