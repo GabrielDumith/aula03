928dfc103d99d61777028e56d8fedd10719f6919
GabrielDumith <gabriel.dumith321@gmail.com>
2025-03-31 14:25:33 -0300

    commit aula03

---
+++ aula03.py
@@ -0,0 +1,6 @@
+nome= input("escreva seu nome: ")
+idade= int(input("escreva sua a idade: "))
+salario= float(input("escreva o seu salario: "))
+
+
+print(f"seu nome é:{nome}, sua idade:{idade}, salario{salario}")
