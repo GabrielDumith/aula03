610cd982a2df134a449ec5128002befae9bf5c48
GabrielDumith <gabriel.dumith321@gmail.com>
2025-04-02 14:26:39 -0300

    gasolina

---
+++ gasolina.py
@@ -0,0 +1,10 @@
+litros=float((input("quantos litros voce abasteceu"))
+tipo=input("qual o tipo de gasolina/n"
+           "g é para gasolina"
+           "e para gasolina")
+vGAS=5.8
+VETA=4.9
+if tipo == "g":
+    valor=litros*vGAS
+    print(f"voce estabeleceu {litros} litros")
+
